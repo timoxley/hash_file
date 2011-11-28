ded97a01bb4ada00b7253c2f8bf8d09325cc6a19
Tim Oxley <secoif@gmail.com>
2011-11-28 19:42:17 +1000

    Fix callback format for 'buffered' version to callback(err, data...) format

--- sha1_file.js
+++ sha1_file.js
@@ -1,67 +1,62 @@
 var fs = require('fs')
 
-var sha1_file_buffered = function(fileName, cb) {
-  require('fs').readFile(fileName,function(err, data){
-    if(err) return cb(err)
-    var sha1 = require('crypto').createHash('sha1');
-    debugger
-    var update = sha1.update(data);
-    var digest = update.digest('hex');
-    cb(digest)
-    //return require('crypto').createHash('sha1').update(data).digest('hex')
-  })   
-};
+/// Buffering Version ///
+var sha1_file_buffered = function(fileName, callback) {
+  require('fs').readFile(fileName, function(err, data){
+    if (err) { return callback(err) }
+    var sha1 = require('crypto').createHash('sha1')
+    var update = sha1.update(data)
+    var digest = update.digest('hex')
+    callback(null, digest)
+  })
+}
 
-//non buffering version
-var sha1_file = function(fileName, cb) {
-  var sha1 = require('crypto').createHash('sha1');
-  fs.stat(fileName, function(err, stats){
-		if(err){
-			return cb(err)
+/// Non-buffering Version ///
+var sha1_file = function(fileName, callback) {
+
+  var sha1 = require('crypto').createHash('sha1')
+
+  fs.stat(fileName, function(err, stats) {
+		if (err) {
+			return callback(err)
 		}
-    fs.open(fileName, 'r', 666 , function(err, fd){
-  		if(err){
-  			return cb(err);
+    fs.open(fileName, 'r', 666 , function(err, fd) {
+  		if (err) {
+  			return cb(err)
   		}
-  		
-		  var read_len = 1024 * 1024 || stats.blksize,
-			  offset = 0;
-      var dat= new Buffer(read_len);
 
-				//Actual do read function
-			var do_read = function(){
-				fs.read(fd, dat, 0, read_len, offset, function(err, readlen){			  
-//			var do_read = function(){
-//        fs.read(fd, read_len, offset, 'binary', function(err, dat){
-  				if(err){
-  					return cb(err);
+		  var read_len = 1024 * 1024 || stats.blksize, offset = 0
+      var data = new Buffer(read_len)
+
+			var do_read = function() {
+				fs.read(fd, data, 0, read_len, offset, function(err, readlen) {
+  				if (err) {
+  					return cb(err)
   				}
-  				
-  				sha1.update(dat); 
-  				  				
-  				offset += read_len;
+  				sha1.update(data)
+
+  				offset += read_len
+
   				//less than blksize bytes left
-  				if( read_len > stats.size - offset && offset < stats.size){
-  					read_len = stats.size - offset; 
-  					dat = new Buffer(read_len);
+  				if (read_len > stats.size - offset && offset < stats.size) {
+  					read_len = stats.size - offset
+  					data = new Buffer(read_len)
   				}
 				
-  				if( offset >= stats.size ){
+  				if(offset >= stats.size) {
             //read complete
-  					fs.closeSync(fd);
+  					fs.closeSync(fd)
   					//Close and go home
-  					var digest = sha1.digest('hex');
-  					return cb(null, digest)
+  					var digest = sha1.digest('hex')
+  					return callback(null, digest)
   				}
-        
-          do_read();
-          //return require('crypto').createHash('sha1').update(data).digest('hex')
-        })  
+          do_read()
+        })
       }
       do_read()
     })
-  }) 
-};
+  })
+}
 
-module.exports = sha1_file;
-module.exports.buffered = sha1_file_buffered;
+module.exports = sha1_file
+module.exports.buffered = sha1_file_buffered
