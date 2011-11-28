c603a7d22e3836bdbc95db7158537d325eaf133e
Tim Oxley <secoif@gmail.com>
2011-11-28 19:41:25 +1000

    Update test to use mocha framework. Need to figure out best way of reporting performance, since performance reporting has been removed for now.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,29 @@
-sha1_file = require('../sha1_file.js')
-util = require('util')
-//var file = '../README.md'
-var file = '/Users/dotmaster/Downloads/Software/Microsoft Office 2011 v14.0.0.dmg'
-stat = require('fs').statSync(file)
-size = stat['size']
-var start = new Date().getTime()
-sha1_file(file, function(err, sha1){
-  if (err) return util.log(err)
-    var end = new Date().getTime()
-  util.log(sha1 + ' rate: ' + (size/(end-start) *1000 /(1024*1024)).toFixed(2) + 'mb/sec')
+var sha1_file = require('../sha1_file.js'),
+    fs = require('fs'),
+    assert = require('assert')
+
+var filePath = process.argv[1] || './sha1_file.js'
+var stat = fs.statSync(filePath)
+var size = stat['size']
+
+describe('sha1_file', function(){
+  it('should generate a sha1 without error', function(done){
+    sha1_file(filePath, function(err, sha1) {
+      assert.ok(!err, err)
+      assert.ok(sha1)
+      assert.equal(40, sha1.length)
+      done()
+    })
+  })
 })
 
+describe('sha1_file_buffered', function(){
+  it('should generate a sha1 without error', function(done){
+    sha1_file.buffered(filePath, function(err, sha1) {
+      assert.ok(!err, err)
+      assert.ok(sha1)
+      assert.equal(40, sha1.length)
+      done()
+    })
+  })
+})
--- test/test_buffered.js
+++
@@ -1,7 +0,0 @@
-sha1_file_buffered = require('../sha1_file.js').buffered
-util = require('util')
-sha1_file_buffered('../README.md', function(err, sha1){
-  if (err) util.log(err)
-  else util.log(sha1)
-})
-
